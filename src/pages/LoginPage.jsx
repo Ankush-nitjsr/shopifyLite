27fa33bb3d5b7ed4dee8eb497c60213059dab2ae
Ankush <ankushnitjsr@gmail.com>
2024-10-09 11:08:31 +0530

    feat: Modify LoginPage component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useState } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 import { toast } from "react-toastify";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
   // authentication handling of user with context hook
@@ -59,24 +60,56 @@ function LoginPage() {
     <>
       <Header />
       <main>
-        <h1>Login Page</h1>
-        <form onSubmit={handleOnLogin} className="login-form">
-          <input
-            name="username"
-            type="text"
-            placeholder="Username"
-            value={user.username}
-            onChange={handleChange}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={user.password}
-            onChange={handleChange}
-          />
-          <button type="submit">Login</button>
-        </form>
+        <div className="border border-gray-400 rounded-2xl flex flex-col h-[60vh] bg-slate-300">
+          <form onSubmit={handleOnLogin} className="login-form">
+            <h1 className="text-2xl mx-auto mt-14 text-black">
+              Login to your account
+            </h1>
+            <p className="flex flex-col space-y-1">
+              <label htmlFor="username" className="text-black text-lg">
+                Username
+              </label>
+              <input
+                name="username"
+                type="text"
+                placeholder="Enter username"
+                value={user.username}
+                onChange={handleChange}
+                className="h-10"
+              />
+            </p>
+            <p className="flex flex-col space-y-1">
+              <label htmlFor="username" className="text-black text-lg">
+                Password
+              </label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={user.password}
+                onChange={handleChange}
+                className="h-10"
+              />
+            </p>
+            <p className="flex flex-col">
+              <button type="submit" className="h-10 text-center bg-blue-600">
+                Login
+              </button>
+              <h3 className="text-xs text-black">
+                Choose username and password from{" "}
+                <Link
+                  className="cursor-pointer text-blue-600 text-sm inline"
+                  to="https://dummyjson.com/users"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  here
+                </Link>{" "}
+                to login
+              </h3>
+            </p>
+          </form>
+        </div>
       </main>
     </>
   );
