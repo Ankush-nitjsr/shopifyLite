14e47b182d23106c32a5aeedec88aff79f2cbaf5
Ankush <ankushnitjsr@gmail.com>
2024-10-28 01:20:26 +0530

    feat: Add PWeight component

---
+++ src/components/product-page/PWeight.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+
+export const PWeight = ({ weight }) => {
+  return (
+    <div className="flex space-x-1">
+      <p className="font-semibold">Product Weight :</p>
+      <p>{weight} g</p>
+    </div>
+  );
+};
+
+PWeight.propTypes = {
+  weight: PropTypes.string,
+};
