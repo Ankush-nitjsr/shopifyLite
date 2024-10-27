769582b4b554bd6322e5a1c72ee8e70b513b7181
Ankush <ankushnitjsr@gmail.com>
2024-10-28 01:05:05 +0530

    refactor: reafactor ProductPage component and rename product folder to product-page

---
+++ src/components/product-page/ProductPage.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { useParams } from "react-router-dom";
+import { useGetProduct } from "./useGetProduct";
+import Header from "../Header";
+import { PPLeftContainer } from "./PPLeftContainer";
+import { PPRightContainer } from "./PPRightContainer";
+
+export const ProductPage = () => {
+  // Get product id from URL params
+  const { pid } = useParams();
+
+  // Get product data
+  const { data } = useGetProduct(pid);
+
+  // Show loading state if data is fetching/loading
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <>
+      <Header />
+      <div className="flex w-full px-7 py-7 justify-center space-x-8">
+        {/* Left Section: Product Images */}
+        <PPLeftContainer
+          title={data.title}
+          images={data.images}
+          thumbnail={data.thumbnail}
+        />
+
+        {/* Right Section: Product Details */}
+        <PPRightContainer data={data} />
+      </div>
+    </>
+  );
+};
