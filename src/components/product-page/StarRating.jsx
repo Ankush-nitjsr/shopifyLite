94534ec63554e6e7faf05aed106fd78b181de585
Ankush <ankushnitjsr@gmail.com>
2024-10-28 23:14:52 +0530

    feat: Add StarRating component
    Add StarRating component to handle display of product star rating

---
+++ src/components/product-page/StarRating.jsx
@@ -0,0 +1,31 @@
+import { StarIcon as SolidStarIcon } from "@heroicons/react/20/solid";
+import { StarIcon as OutlineStarIcon } from "@heroicons/react/24/outline";
+import PropTypes from "prop-types";
+
+export const StarRating = ({ rating }) => {
+  // Get floored value of the rating
+  const ratingStars = Math.floor(rating);
+
+  // Check if the rating has a decimal part for rendering an empty star
+  const hasPartialStar = 5 > ratingStars;
+
+  return (
+    <div className="star-rating flex items-center">
+      {Array(ratingStars)
+        .fill()
+        .map((_, idx) => (
+          <SolidStarIcon key={idx} className="w-5 h-5 text-yellow-500" />
+        ))}
+      {hasPartialStar &&
+        Array(5 - ratingStars)
+          .fill()
+          .map((_, idx) => (
+            <OutlineStarIcon key={idx} className="w-5 h-5 text-yellow-500" />
+          ))}
+    </div>
+  );
+};
+
+StarRating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
