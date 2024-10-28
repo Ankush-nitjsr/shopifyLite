218122a3ba8bd53b8d2b25a74724344c6ab258c8
Ankush <ankushnitjsr@gmail.com>
2024-10-28 23:28:29 +0530

    feat: Add handleClick function prop

--- src/components/product-page/PButton.jsx
+++ src/components/product-page/PButton.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 
-export const PButton = ({ text }) => {
+export const PButton = ({ text, handleClick }) => {
   return (
     <button
       className={`${
         text === "Add to Cart" ? "bg-yellow-500" : "bg-orange-700"
       } rounded-3xl w-60 h-9 text-gray-900 flex items-center justify-center`}
+      onClick={handleClick}
     >
       {text}
     </button>
@@ -13,5 +14,6 @@ export const PButton = ({ text }) => {
 };
 
 PButton.propTypes = {
-  text: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  handleClick: PropTypes.func,
 };
