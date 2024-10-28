34e74a846b175f40ebd510394b7165104c1c4346
Ankush <ankushnitjsr@gmail.com>
2024-10-28 23:25:01 +0530

    refactor: Refactor ProductReviews component
    Use StarRating component to handle rating display

--- src/components/product-page/ProductReviews.jsx
+++ src/components/product-page/ProductReviews.jsx
@@ -1,36 +1,39 @@
 import PropTypes from "prop-types";
-import { StarIcon as SolidStarIcon } from "@heroicons/react/20/solid";
-import { StarIcon as OutlineStarIcon } from "@heroicons/react/24/outline";
+import { StarRating } from "./StarRating";
 
-export const ProductReviews = ({ reviews }) => {
+export const ProductReviews = ({ reviews, rating }) => {
   return (
-    <div className="reviews-section">
-      <p className="font-bold">Customer Reviews:</p>
-      {reviews.map((review, index) => (
-        <div key={index} className="border p-3 rounded mb-3">
-          <p className="font-bold">{review.reviewerName}</p>
-          <div className="flex">
-            <div className="rating text-lg mr-2">{review.rating}</div>
-            <div className="flex">
-              {Array(Math.floor(review.rating))
-                .fill()
-                .map((_, idx) => (
-                  <SolidStarIcon
-                    key={idx}
-                    className="w-4 h-4 text-yellow-500"
-                  />
-                ))}
-              {review.rating % 1 !== 0 && (
-                <OutlineStarIcon className="w-4 h-4 text-yellow-500" />
-              )}
+    <div className="reviews-section space-y-3 my-4">
+      <div>
+        <p className="text-2xl font-bold">Customer reviews:</p>
+      </div>
+
+      {/* Star rating of product */}
+      <div className="flex space-x-2">
+        <StarRating rating={rating} />
+        <p className="text-xl">{`${rating} out of 5`}</p>
+      </div>
+
+      {/* Product Reviews */}
+      <div className="space-y-6">
+        {reviews.map((review, index) => (
+          <div
+            key={index}
+            className="border p-6 rounded-2xl flex space-x-4 items-center w-[60%]"
+          >
+            <div className="w-[20%] space-y-2">
+              <p className="font-bold">{review.reviewerName}</p>
+              <StarRating rating={review.rating} />
+            </div>
+            <div className="space-y-1">
+              <p className="text-sm text-gray-400">
+                Reviewed on: {new Date(review.date).toLocaleDateString()}
+              </p>
+              <p className="text-xl">{review.comment}</p>
             </div>
           </div>
-          <p>{review.comment}</p>
-          <p className="text-sm text-gray-400">
-            Reviewed on: {new Date(review.date).toLocaleDateString()}
-          </p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
@@ -44,4 +47,5 @@ ProductReviews.propTypes = {
       date: PropTypes.string.isRequired,
     })
   ).isRequired,
+  rating: PropTypes.number.isRequired,
 };
