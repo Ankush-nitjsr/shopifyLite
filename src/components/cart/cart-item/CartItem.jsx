5eab233a3ba4914e9f76dbc89497a30eccd579df
Ankush <ankushnitjsr@gmail.com>
2024-10-29 22:50:43 +0530

    feat: Update CartItem component

--- src/components/cart/cart-item/CartItem.jsx
+++ src/components/cart/cart-item/CartItem.jsx
@@ -6,24 +6,27 @@ import {
   PlusCircleIcon,
   TrashIcon,
 } from "@heroicons/react/16/solid";
+import { Link } from "react-router-dom";
+import { Checkbox } from "../../../ui/checkbox";
+import { Separator } from "../../../ui/separator";
+import { PDeliveryAndAvailability } from "../../product-page/PDeliveryAndAvailability";
+import { CheckboxField } from "../../../ui/CheckboxField";
+import { productPropTypes } from "../../../lib/productPropTypes";
 
-const CartItem = ({ rowData }) => {
+const CartItem = ({ product }) => {
   const { myCartData, setMyCartData } = useContext(AuthContext);
 
   const handleIncrease = (id) => {
-    let qty = rowData.productQuantity;
+    let qty = product.quantity;
     qty++;
 
-    let newProductAmount = rowData.productPrice * qty;
-
-    const updatedRowData = {
-      ...rowData,
-      productQuantity: qty,
-      productAmount: newProductAmount,
+    const updatedproduct = {
+      ...product,
+      quantity: qty,
     };
 
     let myUpdatedCartData = myCartData.map((product) =>
-      product.productId === id ? updatedRowData : product
+      product.id === id ? updatedproduct : product
     );
 
     setMyCartData(myUpdatedCartData);
@@ -31,19 +34,16 @@ const CartItem = ({ rowData }) => {
   };
 
   const handleDecrease = (id) => {
-    let qty = rowData.productQuantity;
+    let qty = product.quantity;
     if (qty > 1) qty--;
 
-    let newProductAmount = rowData.productPrice * qty;
-
-    const updatedRowData = {
-      ...rowData,
-      productQuantity: qty,
-      productAmount: newProductAmount,
+    const updatedproduct = {
+      ...product,
+      quantity: qty,
     };
 
     let myUpdatedCartData = myCartData.map((product) =>
-      product.productId === id ? updatedRowData : product
+      product.id === id ? updatedproduct : product
     );
 
     setMyCartData(myUpdatedCartData);
@@ -52,9 +52,7 @@ const CartItem = ({ rowData }) => {
   };
 
   const handleRemove = (id) => {
-    let myUpdatedCartData = myCartData.filter(
-      (product) => product.productId !== id
-    );
+    let myUpdatedCartData = myCartData.filter((product) => product.id !== id);
 
     setMyCartData(myUpdatedCartData);
 
@@ -62,36 +60,95 @@ const CartItem = ({ rowData }) => {
   };
 
   return (
-    <tr key={rowData.productId} id={rowData.productId}>
-      <td>
-        <img
-          className="h-24"
-          src={rowData.productPicture}
-          alt={rowData.productName}
-        />
-      </td>
-      <td>{rowData.productName}</td>
-      <td>{rowData.productPrice}</td>
-      <td>
-        <div className="flex items-center space-x-2">
-          <button onClick={() => handleIncrease(rowData.productId)}>
-            <PlusCircleIcon className="h-6 w-6 text-green-400 transition-transform duration-300 ease-in-out transform hover:scale-125" />
-          </button>
-          <span className="text-lg">{rowData.productQuantity}</span>
-          {rowData.productQuantity > 1 ? (
-            <button onClick={() => handleDecrease(rowData.productId)}>
-              <MinusCircleIcon className="h-6 w-6 text-red-600 transition-transform duration-300 ease-in-out transform hover:scale-125" />
+    <div className="flex justify-between bg-gray-400 p-4 h-56 w-full">
+      <div className="ci-checkbox flex items-center justify-center w-[2%]">
+        <Checkbox />
+      </div>
+
+      <div className="ci-image-wrapper h-full w-[28%]">
+        <Link to={`/product/${product.id}`}>
+          <img
+            className="ci-image h-full w-full"
+            src={product.images[0]}
+            alt={product.title}
+          />
+        </Link>
+      </div>
+
+      <div className="ci-content-group h-full flex flex-col justify-between flex-1">
+        <ul className="flex gap-2 justify-between">
+          <div>
+            <li className="ci-title text-gray-900">{product.title}</li>
+            <div className="ci-details text-gray-700 space-y-2">
+              <li>{product.category}</li>
+              <li>
+                <PDeliveryAndAvailability
+                  shippingInformation={product.shippingInformation}
+                  availabilityStatus={product.availabilityStatus}
+                />
+              </li>
+              <li>
+                <CheckboxField text={"This will be a gift"} />
+              </li>
+            </div>
+          </div>
+
+          <div className="ci-price-wrapper flex flex-col space-y-2">
+            <div className="flex flex-col space-x-2">
+              <div className="discount flex justify-end">
+                <span className="text-gray-100 p-1 rounded bg-red-800">
+                  {-product.discountPercentage}%
+                </span>
+              </div>
+              <div className="flex justify-end">
+                <span className="price text-2xl font-bold text-gray-900">
+                  ${product.price}
+                </span>
+              </div>
+            </div>
+            <div className="flex text-gray-600">
+              <p>M.R.P.:</p>
+              <p className="ml-2 line-through">
+                $
+                {(
+                  product.price /
+                  (1 - product.discountPercentage / 100)
+                ).toFixed(2)}
+              </p>
+            </div>
+          </div>
+        </ul>
+
+        <div className="ci-action-links flex items-center gap-4">
+          <div className="flex items-center justify-center space-x-5 border border-gray-600 rounded-3xl px-1 py-1">
+            {product.quantity > 1 ? (
+              <button onClick={() => handleDecrease(product.id)}>
+                <MinusCircleIcon className="h-6 w-6 text-red-600 transition-transform duration-300 ease-in-out transform hover:scale-125" />
+              </button>
+            ) : (
+              <button onClick={() => handleRemove(product.id)}>
+                <TrashIcon className="h-6 w-6 text-red-600 transition-transform duration-300 ease-in-out transform hover:scale-125" />
+              </button>
+            )}
+            <span className="text-lg text-gray-900">{product.quantity}</span>
+            <button onClick={() => handleIncrease(product.id)}>
+              <PlusCircleIcon className="h-6 w-6 text-green-400 transition-transform duration-300 ease-in-out transform hover:scale-125" />
             </button>
-          ) : (
-            <button onClick={() => handleRemove(rowData.productId)}>
-              <TrashIcon className="h-6 w-6 text-red-600 transition-transform duration-300 ease-in-out transform hover:scale-125" />
-            </button>
-          )}
+          </div>
+          <Separator orientation="vertical" className="bg-gray-500 h-5" />
+          <button className="text-blue-800">Delete</button>
+          <Separator orientation="vertical" className="bg-gray-500 h-5" />
+          <button className="text-blue-800">Save for later</button>
+          <Separator orientation="vertical" className="bg-gray-500 h-5" />
+          <button className="text-blue-800">Share</button>
         </div>
-      </td>
-      <td>{Math.round(rowData.productAmount * 100) / 100}</td>
-    </tr>
+      </div>
+    </div>
   );
+};
+
+CartItem.propTypes = {
+  product: productPropTypes.isRequired,
 };
 
 export default CartItem;
