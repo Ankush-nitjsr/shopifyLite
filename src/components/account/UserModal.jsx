6bae78f895977e68ce2776778d16a2d96d99f14f
Ankush <ankushnitjsr@gmail.com>
2024-11-01 23:57:28 +0530

    feat: Add UserModal component

---
+++ src/components/account/UserModal.jsx
@@ -0,0 +1,25 @@
+import { userPropTypes } from "../../lib/userPropTypes";
+
+export const UserModal = ({ user }) => {
+  console.log("user phone: ", user.phone);
+
+  return (
+    <div className="flex items-center bg-white p-4 shadow-lg rounded-lg space-x-4">
+      <div className="user-avatar">
+        <img src={user.image} alt="user-avatar" className="h-16 w-16" />
+      </div>
+      <div className="greeting">
+        <div>
+          <span className="text-sm">Hello,</span>
+        </div>
+        <div>
+          <span className="text-2xl font-medium">{user.firstName}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+UserModal.propTypes = {
+  user: userPropTypes,
+};
