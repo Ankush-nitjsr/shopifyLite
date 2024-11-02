ffefbde3e8dad77bbc67c74bc3d54fc60e317278
Ankush <ankushnitjsr@gmail.com>
2024-11-02 12:33:51 +0530

    feat: Add logout util function
    function to handle logout user

---
+++ src/lib/logout.js
@@ -0,0 +1,19 @@
+import { toast } from "react-toastify";
+
+/**
+ * function to logout for user
+ * set isAuthenticated to empty string (setIsAuthenticated(""))
+ * when using logout function
+ */
+const logout = () => {
+  // delete user from local storage
+  localStorage.removeItem("userDetails");
+  // delete cart
+  localStorage.removeItem("myCartData");
+  // delete token saved in local storage
+  localStorage.removeItem("token");
+  // display success message on Logout
+  toast.success("Logged out successfully");
+};
+
+export { logout };
