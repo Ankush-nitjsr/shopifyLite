b7e788b36dd3dfa7c8421083f977a332a9666674
Ankush <ankushnitjsr@gmail.com>
2024-11-03 23:42:22 +0530

    chore: Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,113 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{html,js,jsx,ts,tsx,md,mdx}",
+    "./components/**/*.{html,js,jsx,ts,tsx,md,mdx}",
+    "./app/**/*.{html,js,jsx,ts,tsx,md,mdx}",
+    "./src/**/*.{html,js,jsx,ts,tsx,md,mdx}",
+  ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
+    },
+    fontSize: {
+      sm: ["0.8rem", "1.25rem"],
+      base: ["0.95rem", "1.5rem"],
+      lg: ["1.1rem", "1.6rem"],
+      xl: ["1.2rem", "1.75rem"],
+      "2xl": ["1.45rem", "2rem"],
+      "3xl": ["1.825rem", "2.25rem"],
+      "4xl": ["2.2rem", "2.5rem"],
+      "5xl": ["2.95rem", "1"],
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        "3xs": ["0.6rem", "1rem"],
+        "2xs": ["0.7rem", "1rem"],
+        xs: ["0.75rem", "1.1rem"],
+      },
+      screens: {
+        mxs: "280px",
+        msm: "320px",
+        mmd: "375px",
+        mlg: "425px",
+        xs: "500px",
+        "2md": "868px",
+        "3md": "968px",
+        "2lg": "1124px",
+        "3lg": "1224px",
+      },
+      colors: {
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)",
+        },
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)",
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          foreground: "var(--accent-foreground)",
+        },
+        popover: {
+          DEFAULT: "var(--popover)",
+          foreground: "var(--popover-foreground)",
+        },
+        card: {
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
+    },
   },
-  plugins: [],
+  plugins: ["tailwindcss-animate"],
 };
