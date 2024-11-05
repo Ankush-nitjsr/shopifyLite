630321feca3cbf3d87c8513870a0b47cbc2dccea
Ankush <ankushnitjsr@gmail.com>
2024-11-06 00:03:48 +0530

    feat: Add loading, error and pagination in return

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -2,20 +2,44 @@ import { useEffect, useState } from "react";
 
 export const useGetProducts = () => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [pagination, setPagination] = useState({
+    total: 0,
+    skip: 0,
+    limit: 30,
+  });
 
   useEffect(() => {
     const fetchProducts = async () => {
+      setLoading(true);
       try {
         const response = await fetch(`https://dummyjson.com/products`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
         const result = await response.json();
-        setData(result);
+        console.log("products: ", result.products);
+
+        // Set products data and pagination details
+        setData(result.products);
+
+        setPagination({
+          total: result.total,
+          skip: result.skip,
+          limit: result.limit,
+        });
       } catch (error) {
-        console.error("Error fetching product:", error);
+        console.error("Error fetching products:", error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchProducts();
   }, []);
 
-  return { data };
+  return { data, loading, error, pagination };
 };
