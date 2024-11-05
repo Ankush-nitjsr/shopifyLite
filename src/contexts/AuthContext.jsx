799b9ce4821dfcf74a73b662b3adeee478fc7a97
Ankush <ankushnitjsr@gmail.com>
2024-11-05 23:56:59 +0530

    refactor: refactor AuthContext
    Handle authentication related data with AuthContext

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,62 +1,37 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState, useEffect, useMemo } from "react";
 import PropTypes from "prop-types";
 
 export const AuthContext = createContext();
 
-export default function AuthProvider({ children }) {
-  // set state for all products
-  const [products, setProducts] = useState([]);
-  // set state for filters
-  const [filter, setFilter] = useState("");
-  // set state for username and password
-  const [user, setUser] = useState({ username: "", password: "" });
-
-  // set state for user details
-  const [userDetails, setUserDetails] = useState(
-    JSON.parse(localStorage.getItem("userDetails")) || {}
-  );
-
-  const [isAuthenticated, setIsAuthenticated] = useState(
+export function AuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(() =>
     localStorage.getItem("token")
   );
-
-  const [myCartData, setMyCartData] = useState(
-    JSON.parse(localStorage.getItem("myCartData")) || []
+  const [userDetails, setUserDetails] = useState(
+    () => JSON.parse(localStorage.getItem("userDetails")) || {}
   );
 
-  const [cartTotalQuantity, setCartTotalQuantity] = useState(0);
-
-  // Recalculate total quantity and amount when cart data changes
   useEffect(() => {
-    const totalQuantity = myCartData.reduce(
-      (acc, item) => acc + item.quantity,
-      0
-    );
-    setCartTotalQuantity(totalQuantity);
-  }, [myCartData]);
+    if (isAuthenticated) {
+      localStorage.setItem("token", isAuthenticated);
+      localStorage.setItem("userDetails", JSON.stringify(userDetails));
+    } else {
+      localStorage.removeItem("token");
+      localStorage.removeItem("userDetails");
+    }
+  }, [isAuthenticated, userDetails]);
 
-  return (
-    <AuthContext.Provider
-      value={{
-        products,
-        setProducts,
-        filter,
-        setFilter,
-        user,
-        setUser,
-        userDetails,
-        setUserDetails,
-        isAuthenticated,
-        setIsAuthenticated,
-        myCartData,
-        setMyCartData,
-        cartTotalQuantity,
-        setCartTotalQuantity,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
+  const value = useMemo(
+    () => ({
+      isAuthenticated,
+      setIsAuthenticated,
+      userDetails,
+      setUserDetails,
+    }),
+    [isAuthenticated, userDetails]
   );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 AuthProvider.propTypes = {
