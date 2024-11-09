5b5adcbb1f601523217a48aab17cceba73d82fac
Ankush <ankushnitjsr@gmail.com>
2024-11-09 20:51:11 +0530

    fix: Update data initial value to empty array

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useGetProducts = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [pagination, setPagination] = useState({
