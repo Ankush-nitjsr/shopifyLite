a4721b418e7c6bfd0013766717522e9c476fe52c
Ankush <ankushnitjsr@gmail.com>
2024-11-13 00:05:26 +0530

    feat: Add discounts and ratings

--- src/components/product-container/constants.js
+++ src/components/product-container/constants.js
@@ -8,4 +8,20 @@ const priceRanges = [
   { label: "Over $1000", startPrice: 1000, endPrice: Infinity },
 ];
 
-export { priceRanges };
+// Discount options
+const discounts = [
+  { label: "5% Off or more", value: 5 },
+  { label: "10% Off or more", value: 10 },
+  { label: "15% Off or more", value: 15 },
+  { label: "20% Off or more", value: 20 },
+];
+
+// Rating options
+const ratings = [
+  { label: "1 ⭐ & above", value: 1 },
+  { label: "2 ⭐ & above", value: 2 },
+  { label: "3 ⭐ & above", value: 3 },
+  { label: "4 ⭐ & above", value: 4 },
+];
+
+export { priceRanges, discounts, ratings };
