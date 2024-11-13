74b233f51eca502f21934abe81d1e6a9861b6231
Ankush <ankushnitjsr@gmail.com>
2024-11-13 17:07:07 +0530

    feat: Improved UI & Add button to navigate to homePage

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -4,11 +4,15 @@ import { ProductContext } from "../../contexts/ProductContext";
 import CartItem from "./cart-item/CartItem";
 import { Separator } from "../../ui/separator";
 import { CartSubTotal } from "./cart-item/CartSubTotal";
+import Button from "../../ui/buttons/Button";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { myCartData } = useContext(ProductContext);
   const { setMyCartData } = useContext(ProductContext);
   const { setCartTotalQuantity } = useContext(ProductContext);
+
+  const navigate = useNavigate();
 
   console.log("cart data", myCartData);
 
@@ -31,12 +35,22 @@ const Cart = () => {
   return (
     <div className="w-full flex justify-center gap-6">
       <div className="cart-items w-[60%] bg-white shadow-lg rounded-lg p-2">
-        {myCartData.map((product, i) => (
-          <div key={i}>
-            <CartItem product={product} />
-            <Separator className="bg-gray-200" />
+        {myCartData.length ? (
+          myCartData.map((product, i) => (
+            <div key={i}>
+              <CartItem product={product} />
+              <Separator className="bg-gray-200" />
+            </div>
+          ))
+        ) : (
+          <div className="flex flex-col justify-center items-center h-full space-y-4">
+            <p className="text-xl">Your cart is empty!</p>
+            <p className="text-sm">Add items to it now.</p>
+            <Button className="px-16" onClick={() => navigate("/home")}>
+              Shop now
+            </Button>
           </div>
-        ))}
+        )}
       </div>
       {/* Component for Cart total amount at checkout */}
       <CartSubTotal />
