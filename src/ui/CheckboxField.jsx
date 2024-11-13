3e77cc2e16c5ebf7c9442507c610ad59825d0764
Ankush <ankushnitjsr@gmail.com>
2024-11-13 15:22:29 +0530

    feat: Add more props inline with Checkbox element

--- src/ui/CheckboxField.jsx
+++ src/ui/CheckboxField.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
 import { Checkbox } from "../ui/checkbox";
 
-export function CheckboxField({ text }) {
+export function CheckboxField({ text, checked, onCheckedChange }) {
   return (
     <div className="ml-0.5 flex items-center space-x-2">
-      <Checkbox id="terms" />
+      <Checkbox
+        id="terms"
+        checked={checked}
+        onCheckedChange={onCheckedChange}
+      />
       <label
         htmlFor="terms"
         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -17,4 +21,6 @@ export function CheckboxField({ text }) {
 
 CheckboxField.propTypes = {
   text: PropTypes.string,
+  checked: PropTypes.bool,
+  onCheckedChange: PropTypes.func,
 };
