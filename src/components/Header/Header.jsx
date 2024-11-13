c0b6853f1f96e8e15027802c3d94eed9ccf2122e
Ankush <ankushnitjsr@gmail.com>
2024-11-13 18:44:28 +0530

    feat: Implement logout feature

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
 import { useState, useEffect, useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { ProductContext } from "../../contexts/ProductContext";
 import "./styles.css";
+import Button from "../../ui/buttons/Button";
+import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/16/solid";
+import { logout } from "../../lib/logout";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Header = () => {
   const { cartTotalQuantity } = useContext(ProductContext);
   const [isScrolling, setIsScrolling] = useState(false);
+  const { setIsAuthenticated } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout(); // execute logout function
+    setIsAuthenticated(""); // Clear authentication
+    navigate("/"); // navigate to login page
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -35,9 +48,16 @@ const Header = () => {
         <NavLink to="/cart" className="nav-link">
           Cart ({`${cartTotalQuantity} items`})
         </NavLink>
-        <NavLink to="/" className="nav-link">
-          {!localStorage.getItem("userDetails") ? "Login" : "Logout"}
-        </NavLink>
+        {localStorage.getItem("userDetails") && (
+          <Button
+            onClick={handleLogout}
+            variant="link"
+            className="nav-link gap-2 text-black"
+          >
+            <ArrowRightStartOnRectangleIcon className="w-5 h-5" />
+            <span>Logout</span>
+          </Button>
+        )}
       </nav>
     </header>
   );
