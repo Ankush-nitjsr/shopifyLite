551542700a3fb7ce9339996304e8638a486ea046
Ankush <ankushnitjsr@gmail.com>
2024-11-14 16:59:22 +0530

    feat: Add searchProduct component to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,8 +9,14 @@ import {
 } from "@heroicons/react/24/outline";
 import { logout } from "../../lib/logout";
 import { AuthContext } from "../../contexts/AuthContext";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
+import { SearchProduct } from "../search-product/SearchProduct";
+import { useGetProducts } from "../../hooks/useGetProducts";
+import { productPropTypes } from "../../lib/productPropTypes";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = ({ setSearchFlag, setSearchedProducts }) => {
+  const { data } = useGetProducts();
   const { cartTotalQuantity } = useContext(ProductContext);
   const [isScrolling, setIsScrolling] = useState(false);
   const { setIsAuthenticated } = useContext(AuthContext);
@@ -42,10 +48,22 @@ const Header = () => {
   return (
     <header className={isScrolling ? "scrolled" : ""}>
       <nav>
-        <NavLink to="/profile" className="nav-link">
+        <NavLink className="flex items-center">
+          <ShoppingBagIcon className="w-10 h-10 text-[#ffa500]" />
+          <p className="font-medium text-xl pt-1">
+            <span className="text-4xl text-[#ffa500]">S</span>
+            <span>hop</span>
+          </p>
+        </NavLink>
+        <SearchProduct
+          allProducts={data}
+          setSearchFlag={setSearchFlag}
+          setSearchedProducts={setSearchedProducts}
+        />
+        <NavLink to="/profile" className="nav-link whitespace-nowrap">
           Profile Details
         </NavLink>
-        <NavLink to="/home" className="nav-link">
+        <NavLink to="/home" className="nav-link whitespace-nowrap">
           Home Page
         </NavLink>
         <NavLink to="/cart" className="nav-link flex gap-1 relative">
@@ -71,6 +89,11 @@ const Header = () => {
       </nav>
     </header>
   );
+};
+
+Header.propTypes = {
+  setSearchFlag: PropTypes.bool,
+  setSearchedProducts: Array.of(productPropTypes),
 };
 
 export default Header;
