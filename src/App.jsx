6952a92f7f30f1e807ecb5ab07e6acf0246c19ab
Ankush <ankushnitjsr@gmail.com>
2024-11-18 02:13:19 +0530

    refactor: Update public and private routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,22 @@ import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import PrivateRoutes from "./components/PrivateRoutes";
 import AccountPage from "./pages/AccountPage";
-import { useContext } from "react";
-import { AuthContext } from "./contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ProductPage } from "./components/product-page/ProductPage";
 
 function App() {
-  const { isAuthenticated } = useContext(AuthContext);
-
   return (
     <div>
       <ToastContainer />
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={!isAuthenticated ? <LoginPage /> : <HomePage />}
-          />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/product/:pid" element={<ProductPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route element={<PrivateRoutes />}>
-            <Route path="/home" element={<HomePage />} />
             <Route path="/profile" element={<AccountPage />} />
             <Route path="/cart" element={<CartPage />} />
-            <Route path="/product/:pid" element={<ProductPage />} />
           </Route>
         </Routes>
       </Router>
