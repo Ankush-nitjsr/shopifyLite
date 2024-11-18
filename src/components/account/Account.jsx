75825c137724af256ef18a0e9dc171739627e0a3
Ankush <ankushnitjsr@gmail.com>
2024-11-18 20:11:52 +0530

    feat: Add visibleAccountSection prop to highlight the selected subSection

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -18,7 +18,10 @@ export const Account = () => {
     <div className="user-account flex w-full bg-gray-200 space-x-5 px-2 py-6 justify-center text-black">
       <div className="left-container w-[20%] space-y-5">
         <UserModal user={user} />
-        <AccountNavMenu setVisibleAccountSection={setVisibleAccountSection} />
+        <AccountNavMenu
+          visibleAccountSection={visibleAccountSection}
+          setVisibleAccountSection={setVisibleAccountSection}
+        />
       </div>
       <div className="right-container w-[60%] bg-white p-8 shadow-lg rounded-lg">
         <AccountDetails user={user} accountSection={visibleAccountSection} />
