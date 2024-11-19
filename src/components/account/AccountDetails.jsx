9d1781c47cbf12a81ec1f82cb1f310b6f8554e2a
Ankush <ankushnitjsr@gmail.com>
2024-11-19 17:01:07 +0530

    feat: Use userAddress component to manage user addresses

--- src/components/account/AccountDetails.jsx
+++ src/components/account/AccountDetails.jsx
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
 import { userPropTypes } from "../../lib/userPropTypes";
 import { PersonalInformation } from "./PersonalInformation";
+import { UserAddress } from "./UserAddress";
 
 export const AccountDetails = ({ user, accountSection }) => {
   return (
     <div>
       {accountSection === "Personal Information" && (
         <PersonalInformation user={user} />
+      )}
+      {accountSection === "Manage Addresses" && (
+        <UserAddress
+          name={`${user.firstName} ${user.lastName}`}
+          address={user.address}
+          phone={user.phone}
+        />
       )}
     </div>
   );
