74db0cca2b7fe765ced2c37a3bdb82c67b8629b8
Ankush <ankushnitjsr@gmail.com>
2024-11-29 18:08:09 +0530

    feat: Implement handleCheckout function
    Added the handleCheckout function to navigate users to the
    checkout page when the "Proceed to Buy" button is clicked.

--- src/components/cart/cart-item/CartSubTotal.jsx
+++ src/components/cart/cart-item/CartSubTotal.jsx
@@ -3,9 +3,14 @@ import { ProductContext } from "../../../contexts/ProductContext";
 import { CheckboxField } from "../../../ui/CheckboxField";
 import { PButton } from "../../product-page/PButton";
 import { Separator } from "../../../ui/separator";
+import { useNavigate } from "react-router-dom";
 
 export const CartSubTotal = () => {
   const { myCartData } = useContext(ProductContext);
+
+  // use navigate hook
+  const navigate = useNavigate();
+
   const { totalPrice, totalQuantity, totalDiscount } = myCartData.reduce(
     (accumulator, product) => {
       const productPrice =
@@ -19,8 +24,14 @@ export const CartSubTotal = () => {
     },
     { totalPrice: 0, totalQuantity: 0, totalDiscount: 0 }
   );
+
+  const handleCheckout = () => {
+    console.log("Checkout initiated");
+    navigate("/checkout");
+  };
+
   return (
-    <div className="bg-white cart-checkout w-[20%] px-6 py-4 space-y-3 shadow-lg rounded-lg max-h-fit">
+    <div className="bg-white cart-checkout w-[20%] px-6 py-4 space-y-3 shadow-lg rounded-lg max-h-fit sticky top-[5.75rem]">
       <h1 className="text-xl font-semibold text-gray-600">PRICE DETAILS</h1>
       <Separator className="bg-gray-500" />
       <div className="text-gray-900 flex justify-between">
@@ -41,7 +52,7 @@ export const CartSubTotal = () => {
         </span>
       </div>
       <div className="flex justify-center">
-        <PButton text={"Proceed to Buy"} />
+        <PButton text={"Proceed to Buy"} handleClick={handleCheckout} />
       </div>
     </div>
   );
