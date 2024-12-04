0ab359bb4d34f719e38560c4e32f34d78d60d735
Ankush <ankushnitjsr@gmail.com>
2024-12-05 00:29:01 +0530

    style: Update for mobile view UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import Button from "../../ui/buttons/Button";
 import {
   ArrowRightStartOnRectangleIcon,
   ShoppingCartIcon,
+  UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { logout } from "../../lib/logout";
 import { AuthContext } from "../../contexts/AuthContext";
@@ -61,58 +62,79 @@ const Header = ({ setSearchFlag, setSearchedProducts }) => {
   }, []);
 
   return (
-    <header className={isScrolling ? "scrolled" : ""}>
+    <header className={`${isScrolling ? "scrolled" : ""} `}>
       <nav>
+        {/* Logo */}
         <NavLink
           to="/home"
           onClick={resetToDefault}
-          className="flex items-center"
+          className="nav-link flex items-center"
         >
-          <ShoppingBagIcon className="w-10 h-10 text-[#ffa500]" />
-          <p className="font-medium text-xl pt-1">
-            <span className="text-4xl text-[#ffa500]">S</span>
-            <span>hop</span>
+          <ShoppingBagIcon className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 text-[#ffa500]" />
+          <p className="pt-1">
+            <span className="text-2xl md:text-4xl font-medium text-[#ffa500]">
+              S
+            </span>
+            <span className="text-lg font-medium md:text-xl">hop</span>
           </p>
         </NavLink>
-        <SearchProduct
-          allProducts={data}
-          setSearchFlag={setSearchFlag}
-          setSearchedProducts={setSearchedProducts}
-        />
+
+        {/* SearchProduct Component */}
+        <div className="search-bar">
+          <SearchProduct
+            allProducts={data}
+            setSearchFlag={setSearchFlag}
+            setSearchedProducts={setSearchedProducts}
+          />
+        </div>
+
+        {/* Profile */}
         <NavLink to="/profile" className="nav-link whitespace-nowrap">
           <div className="flex flex-col">
             {localStorage.getItem("userSession") && userSession.firstName ? (
               <>
-                <span className="text-2xs">{`Hello, ${userSession.firstName}`}</span>
-                <span className="text-sm font-medium">Account & Lists</span>
+                <div className="hidden md:flex flex-col">
+                  <span className="text-3xs md:text-2xs">{`Hello, ${userSession.firstName}`}</span>
+                  <span className="sm:text-xs md:text-sm font-medium">
+                    Account & Lists
+                  </span>
+                </div>
+                <div className="md:hidden flex items-center">
+                  <UserCircleIcon className="w-5 h-5" />
+                  <span className="text-sm">{`${userSession.firstName}`}</span>
+                </div>
               </>
             ) : (
               <>
-                <span className="text-2xs">Hello, log in</span>
-                <span className="text-sm font-medium">Account & Lists</span>
+                <span className="text-3xs md:text-2xs">Hello, log in</span>
+                <span className="hidden sm:text-xs md:text-sm font-medium">
+                  Account & Lists
+                </span>
               </>
             )}
           </div>
         </NavLink>
 
+        {/* Cart */}
         <NavLink to="/cart" className="nav-link flex gap-1 relative">
-          {/* Display cartTotalQuantity only if greater than zero */}
           {cartTotalQuantity > 0 && (
-            <p className="cart-badge w-5 h-5 rounded-md border border-black flex justify-center items-center">
+            <p className="cart-badge w-4 h-4 md:w-5 md:h-5 rounded-md border border-black flex justify-center items-center">
               {cartTotalQuantity}
             </p>
           )}
-          <ShoppingCartIcon className="w-6 h-6" />
-          <p>Cart</p>
+          <ShoppingCartIcon className="w-5 h-5 md:w-6 md:h-6" />
+          <p className="hidden md:block">Cart</p>
         </NavLink>
+
+        {/* Logout */}
         {localStorage.getItem("userSession") && (
           <Button
             onClick={handleLogout}
             variant="link"
             className="nav-link gap-1 text-black"
           >
-            <ArrowRightStartOnRectangleIcon className="w-6 h-6" />
-            <span>Logout</span>
+            <ArrowRightStartOnRectangleIcon className="w-5 h-5 md:w-6 md:h-6" />
+            <span className="hidden md:block">Logout</span>
           </Button>
         )}
       </nav>
