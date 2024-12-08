643d666daecaf5d8c2eafd4703b2c51532d9c58f
Ankush <ankushnitjsr@gmail.com>
2024-12-08 22:21:14 +0530

    refactor: assign checkbox and label dynamic id

--- src/ui/CheckboxField.jsx
+++ src/ui/CheckboxField.jsx
@@ -2,15 +2,13 @@ import PropTypes from "prop-types";
 import { Checkbox } from "../ui/checkbox";
 
 export function CheckboxField({ text, checked, onCheckedChange }) {
+  // Generate a unique id based on the text
+  const id = `checkbox-${text.replace(/\s+/g, "-").toLowerCase()}`;
   return (
     <div className="ml-0.5 flex items-center space-x-2">
-      <Checkbox
-        id="terms"
-        checked={checked}
-        onCheckedChange={onCheckedChange}
-      />
+      <Checkbox id={id} checked={checked} onCheckedChange={onCheckedChange} />
       <label
-        htmlFor="terms"
+        htmlFor={id}
         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
       >
         {text}
