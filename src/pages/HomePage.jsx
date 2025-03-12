e2511960ac2ad2205ce630e16d6a828129c3345a
Ankush <ankushnitjsr@gmail.com>
2025-03-12 23:36:05 +0530

    feat: lazy load LandingPage component in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,57 +1,15 @@
-import { useContext, useEffect, useState } from "react";
-import ProductsContainer from "../components/products-container/ProductsContainer";
-import Header from "../components/Header/Header";
-import { ProductContext } from "../contexts/ProductContext";
-import { useGetProducts } from "../hooks/useGetProducts";
+import { lazy, Suspense } from "react";
+
+// Lazy load the LandingPage component
+const LandingPage = lazy(() =>
+  import("../components/landing-page/LandingPage")
+);
 
 const HomePage = () => {
-  const { data, loading } = useGetProducts();
-  const { products, setProducts } = useContext(ProductContext);
-  const [searchFlag, setSearchFlag] = useState(false);
-  const [searchedProducts, setSearchedProducts] = useState([]);
-
-  useEffect(() => {
-    if (data.length > 0) {
-      setProducts(data);
-    }
-  }, [data, setProducts]);
-
   return (
-    <>
-      <Header
-        setSearchFlag={setSearchFlag}
-        setSearchedProducts={setSearchedProducts}
-      />
-      {loading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "16rem",
-          }}
-        >
-          <h1 className="text-3xl">Loading...</h1>
-        </div>
-      ) : (
-        <div className="w-full bg-gray-200 px-2 py-4 min-h-screen">
-          {(!searchFlag
-            ? products.length > 0
-              ? products
-              : data
-            : searchedProducts
-          ).length > 0 ? (
-            <ProductsContainer
-              productsData={!searchFlag ? products : searchedProducts}
-            />
-          ) : (
-            <div className="h-40 mx-auto mt-4 w-[83%] flex justify-center items-center bg-white p-4 shadow-lg rounded-lg text-gray-500 text-xl">
-              No products found.
-            </div>
-          )}
-        </div>
-      )}
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LandingPage />
+    </Suspense>
   );
 };
 
