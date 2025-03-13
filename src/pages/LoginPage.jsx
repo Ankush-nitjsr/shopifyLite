23d6b6d4eb416c81bdbb6802c03a70e07e46be3e
Ankush <ankushnitjsr@gmail.com>
2025-03-13 14:50:03 +0530

    feat: Implement lazy loading for Header and LoginForm components in LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,27 @@
-import Header from "../components/Header/Header";
-import { LoginForm } from "../components/login/LoginForm";
+import { lazy, Suspense } from "react";
+
+const Header = lazy(() => import("../components/Header/Header"));
+const LoginForm = lazy(() => import("../components/login/LoginForm"));
+
+/**
+ * LoginPage component renders the login page with a header and a login form.
+ * It uses React's Suspense to display a fallback loading indicator while the components are being loaded.
+ *
+ * @component
+ * @example
+ * return (
+ *   <LoginPage />
+ * )
+ */
 
 function LoginPage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Header />
       <div className="flex justify-center items-center min-h-[calc(100vh-80px)] bg-gray-100">
         <LoginForm />
       </div>
-    </>
+    </Suspense>
   );
 }
 
