af4a2500db6381cd73ef1d30f3fed813acdb48f1
Ankush <ankushnitjsr@gmail.com>
2025-03-13 14:50:41 +0530

    feat: Implement lazy loading for Header and Cart components in CartPage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,14 +1,29 @@
-import Header from "../components/Header/Header";
-import Cart from "../components/cart/Cart";
+import { lazy, Suspense } from "react";
+
+const Header = lazy(() => import("../components/Header/Header"));
+const Cart = lazy(() => import("../components/cart/Cart"));
+
+/**
+ * CartPage component that lazily loads the Header and Cart components.
+ *
+ * This component uses React's Suspense to display a fallback loading message
+ * while the Header and Cart components are being loaded.
+ *
+ * @component
+ * @example
+ * return (
+ *   <CartPage />
+ * )
+ */
 
 const CartPage = () => {
   return (
-    <div className="">
+    <Suspense fallback={<div>Loading...</div>}>
       <Header />
       <main className="p-6 bg-gray-200 min-h-[calc(100vh-80px)]">
         <Cart />
       </main>
-    </div>
+    </Suspense>
   );
 };
 
